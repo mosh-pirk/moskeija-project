d800f3b7e03293e8847207828c9a647e6e2c133f
Mohamid Shaker <mohamid.shaker.code@gmail.com>
2023-07-10 18:52:32 +0300

    login palauttaa token

--- server/src/tests/login_api.test.js
+++ server/src/tests/login_api.test.js
@@ -4,22 +4,25 @@ const app = require('../app')
 const api = supertest(app)
 const Person = require('../models/person')
 const User = require("../models/user");
-const {testUser, validUser} = require("./helper/data");
+const {testUser} = require("./helper/data");
 
 describe('Login test', () => {
-    test('Käyttäjä kirjautuu sisään ja saa token', async () => {
-        const response = await api.get('/api/users')
 
-        console.log(response.body)
+    test('käyttäjä rekistroi järjestelmään onnistueesti', async () => {
+        const user = await api.post('/api/users').send(testUser).expect(201)
+        expect(user.body.username).toBe(testUser.username)
+    })
+    test('Käyttäjä kirjautuu sisään ja saa token', async () => {
+        const response = await api.post('/api/login').send(testUser)
+
+        expect(response.body.token).toBeTruthy()
+        expect(response.body.username).toEqual(testUser.username)
 
     })
 
     beforeAll(async () => {
         await User.deleteMany({})
         await Person.deleteMany({})
-        console.log(validUser())
-        const user = await User(validUser())
-        //user.save()
     })
 
     afterAll(async () => {
